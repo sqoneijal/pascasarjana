80b9c095d87f127a884f3621ccccda7ab28ddbe3
sqoneijal <sqone.developer@gmail.com>
2024-06-02 20:03:54 +0700

    mahasiswa

--- tugas-akhir/login/build.sh
+++ tugas-akhir/login/build.sh
@@ -8,12 +8,9 @@ hash_css=$(openssl sha1 < ../../cdn/bundle/tugas-akhir/login/App.css | awk '{pri
 cp ../../cdn/bundle/tugas-akhir/login/App.js ../../cdn/bundle/tugas-akhir/login/app.$hash_js.js
 cp ../../cdn/bundle/tugas-akhir/login/App.css ../../cdn/bundle/tugas-akhir/login/app.$hash_css.css
 
-# Menulis hasil hash ke file Common.php
-
-echo "define('HASH_JS', '$hash_js');" >> app/Common.php.sample
-echo "define('HASH_CSS', '$hash_css');" >> app/Common.php.sample
-
 cp app/Common.php.sample app/Common.php
+echo "define('HASH_JS', '$hash_js');" >> app/Common.php
+echo "define('HASH_CSS', '$hash_css');" >> app/Common.php
 
 rm ../../cdn/bundle/tugas-akhir/login/App.js
 rm ../../cdn/bundle/tugas-akhir/login/App.css
