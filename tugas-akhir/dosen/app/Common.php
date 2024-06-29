56965a6967c80134f218416d56b8353cfbe5ae44
sqoneijal <sqone.developer@gmail.com>
2024-06-29 20:45:32 +0700

    tugas akhir > dosen

--- tugas-akhir/dosen/app/Common.php
+++ tugas-akhir/dosen/app/Common.php
@@ -2,5 +2,5 @@
 
 define('LOGIN_URL', 'http://localhhost:8013/');
 define('JWT_KEY', 'gB?RR<SiE!@g~;wYPd7.xlNj&D3@6Q39xB_8+t!U|o1LH7L@T:@37^JVAH3?{Zq');
-define('HASH_JS', '671ea1302cb657c11a2cf5b51b90f054894b56d7');
-define('HASH_CSS', 'f7eb8588e85a3502971778807b690ad4734dd4b4');
+define('HASH_JS', '');
+define('HASH_CSS', '');
