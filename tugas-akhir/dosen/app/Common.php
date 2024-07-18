75ee9edaf7940ccdd3e764c8f2edc6086d0809e5
sqoneijal <sqone.developer@gmail.com>
2024-07-18 10:47:05 +0700

    refresh .gitignore

--- tugas-akhir/dosen/app/Common.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define('LOGIN_URL', 'http://localhost:8013/');
-define('JWT_KEY', '');
-define('HASH_JS', '310bd02ae81e24904554d2717009a0120d833712');
-define('HASH_CSS', 'f7eb8588e85a3502971778807b690ad4734dd4b4');
--- tugas-akhir/login/app/Common.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define('CDN_PATH', 'http://localhost:8012/');
-define('JWT_KEY', '');
-define('HASH_JS', '3984cf0660957b0b0df15c2ff071012ddd7f4e1f');
-define('HASH_CSS', 'de2c8499adb45212f8148d80c3a96ee4076a994c');
