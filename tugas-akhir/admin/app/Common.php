950078b6cc688e52be5807773ed6ed59292c66ae
sqoneijal <sqone.developer@gmail.com>
2024-07-18 10:32:37 +0700

    common

--- tugas-akhir/admin/app/Common.php
+++ tugas-akhir/admin/app/Common.php
@@ -3,7 +3,7 @@
 define('CDN_PATH', 'http://localhost:8012/');
 define('JWT_KEY', '');
 define('LOGIN_PAGE', 'http://localhost:8013/');
-define('FEEDER_PATHNAME', 'https://neofeeder.ar-raniry.ac.id/ws/live2.php');
+define('FEEDER_PATHNAME', '');
 define('FEEDER_USERNAME', '');
 define('FEEDER_PASSWORD', '');
 define('HASH_JS', 'bd7c8b88c2bf4193298c4fa4fd6f2c282dcd62ad');
